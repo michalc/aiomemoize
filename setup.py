dfad86b84d0bd1ae40b8afa3c9b5f8aeb35041db
Michal Charemza <michal@charemza.name>
2019-03-31 12:21:28 +0100

    (feat) Initial behaviour

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='aiomemoize',
+    version='0.0.0',
+    author='Michal Charemza',
+    author_email='michal@charemza.name',
+    description='Memoize asyncio Python function calls',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michalc/aiomemoize',
+    py_modules=[
+        'aiomemoize',
+    ],
+    python_requires='~=3.5',
+    test_suite='test',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: AsyncIO',
+    ],
+)
