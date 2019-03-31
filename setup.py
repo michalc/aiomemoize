087edce619bc1a1f02a44b770486c1f0bf14a33b
Michal Charemza <michal@charemza.name>
2019-03-31 12:34:36 +0100

    (release) v0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='aiomemoize',
-    version='0.0.1',
+    version='0.0.2',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='Memoize asyncio Python function calls',
